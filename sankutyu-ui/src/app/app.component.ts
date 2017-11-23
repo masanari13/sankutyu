2c3fb111d873cee40d0be57c67876db84921057a
masanari13 <nitorikunn13@gmail.com>
2017-11-23 23:04:57 +0900

    bowerの追加

--- sankutyu-ui/src/app/app.component.ts
+++ sankutyu-ui/src/app/app.component.ts
@@ -1,7 +1,72 @@
 import { Component } from '@angular/core';
+import {DomSanitizer,SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{getName()}}</h1>
+    <p class="testClass" [class]="greetingClass">{{greeting}}</p>
+    <p class="testClass" [class.greeting]=flagTrue>{{member?.name}}</p>
+    <img [src]="image" />
+    <img bind-src="image" />
+    <div [innerHTML]="safeMsg"></div>
+    <table>
+      <tr [style.background-color]="bcolor" [style.font-size.%]="fontLsize">
+        <td [attr.rowspan]="len" [innerHtml]="test"></td>
+      </tr>
+      <tr>
+        <td [innerHtml]="test"></td>
+      </tr>
+      <tr>
+        <td [innerHtml]="test"></td>
+      </tr>
+    </table>
+    <input type="button" (click)="show($event)" value="現在時刻" />
+    <p>{{currentTime}}</p>
+    <p><input type="text" (input)="showInput($event)"></p>
+    <ul [innerHtml]="inputEvent"></ul>
+    `,
+    styles: [`
+      .greeting{
+        color:#ddd;
+      }
+      .testClass{
+        font-size:200%;
+      }
+    `]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  test:String = "Stringtest";
+  name:String = 'Angular';
+  greeting:String = 'こんにちは';
+  member = {
+    name: 'ham',
+    age: '27'
+  }
+  msg:String = '<a onClick="alert(\'test\');test()">test</a>'
+  image:String ='http://www.wings.msn.to/image/wings.jpg';
+  len:number=3;
+  greetingClass:String="greeting";
+  flagTrue:boolean=true;
+  bcolor="#0ff"
+  fontLsize:number=300;
+  currentTime:String="-------";
+  inputEvent:String="";
+  constructor(private sanitizer:DomSanitizer){
+    this.safeMsg = sanitizer.bypassSecurityTrustHtml(this.msg);
+  }
+  show(event:any){
+    this.currentTime = new Date().toLocaleString();
+    console.log(event);
+  }
+  showInput(event:any){
+    this.inputEvent += `<li>${event.target.value}</li>` 
+  }
+  getName():String{
+    return this.name;
+  }
+
+  getGreeting():String{
+    return greeting;
+  }
+}
